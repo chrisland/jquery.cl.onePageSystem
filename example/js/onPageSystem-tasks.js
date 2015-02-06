d58c71195fb68ce9cc5e16586a3e57441bf1deac
Christian Marienfeld <post@chrisland.de>
2015-02-06 05:52:08 +0100

    changed folder structure

---
+++ example/js/onPageSystem-tasks.js
@@ -0,0 +1,46 @@
+
+/*
+	
+	onPageSystem - tasks
+	
+	
+	Christian Marienfeld
+	www.chrisland.de
+	
+	Version 1.3.0
+	https://github.com/chrisland/onePageOPStem
+	
+	
+	Licence MIT
+
+*/
+
+
+
+
+/*
+*	Task 
+*/
+
+
+
+OPS.task = (function(){
+	var tasks = {
+
+
+		myFunc: function (page, content, e) {
+			
+			// paste your code here...
+			
+			console.log('myFunc !','page:',page,'content:',content,'e:',e);
+			
+			return true;
+		}
+		
+		
+	};
+	return tasks;
+}());
+
+
+
